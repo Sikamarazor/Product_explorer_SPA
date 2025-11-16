31b120385401e1da0e4d0e3fc41fa83850aa15f5
Lefa Nyathi <Sikamarazor>
2025-11-16 23:30:10 +0200

    Added navigation bar for the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,11 @@ export const routes: Routes = [
             import('./components/home/home.component').then(m => m.HomeComponent),
     },
     {
+        path: 'favorites',
+        loadComponent: () => import('./components/favourite-view/favourite-view.component')
+        .then(m => m.FavouriteViewComponent)
+    },
+    {
         path: 'admin',
         loadChildren: () => import('./components/admin-view/admin.routes')
         .then(m => m.ADMIN_ROUTES)
---
+++ src/app/components/favourite-view/favourite-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-view',
+  imports: [],
+  templateUrl: './favourite-view.component.html',
+  styleUrl: './favourite-view.component.scss'
+})
+export class FavouriteViewComponent {
+
+}
