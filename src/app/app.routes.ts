f1ed9ab56eea61b969daf27165a8754e3b4b8f7b
Lefa Nyathi <Sikamarazor>
2025-11-16 21:58:55 +0200

    Added the home component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),},
+    { path: '**', redirectTo: 'home' },
+];
