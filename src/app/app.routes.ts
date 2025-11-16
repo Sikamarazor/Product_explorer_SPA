2c32a2d43fffff3aebb1f0113815808ec4ffb3ef
Lefa Nyathi <Sikamarazor>
2025-11-16 22:19:50 +0200

    Removed direct import of HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent),},
-    { path: '**', redirectTo: 'home' },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./components/home/home.component').then(m => m.HomeComponent),
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
 ];
