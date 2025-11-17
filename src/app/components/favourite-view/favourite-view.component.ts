aebd8d5688184d10d1a9d2df5decb8631970076a
Lefa Nyathi <Sikamarazor>
2025-11-17 06:51:30 +0200

    Added the favorite view

--- src/app/components/favourite-view/favourite-view.component.ts
+++ src/app/components/favourite-view/favourite-view.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductManagementService } from '../../services/product-management.service';
+import { CommonModule, NgFor, AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-favourite-view',
-  imports: [],
+  imports: [CommonModule, NgFor, AsyncPipe, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './favourite-view.component.html',
   styleUrl: './favourite-view.component.scss'
 })
 export class FavouriteViewComponent {
 
+  public productState = inject(ProductManagementService);
+  favorites$ = this.productState.products$.pipe(
+      map(products =>
+        products.filter(p => this.productState.isFavorite(p.id))
+      )
+    );
+  
+  ngOnInit() {
+    
+  }
+
+  toggleFavorite(productId: string) {
+    this.productState.toggleFavorite(productId);
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule, NgFor, AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ProductManagementService } from '../../services/product-management.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, NgFor, AsyncPipe, MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+   public productState = inject(ProductManagementService);
 
+   products$ = this.productState.products$;
+
+  constructor() {}
+
+  toggleFavorite(productId: string) {
+    this.productState.toggleFavorite(productId);
+  }
 }
